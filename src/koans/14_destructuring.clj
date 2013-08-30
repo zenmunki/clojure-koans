d65aec12501220fa5f7bae5d6764ed6d5ddb9ad9
Malcolm Inglis <me@minglis.id.au>
2013-08-30 02:06:12 -0700

    Added my solutions.

--- src/koans/14_destructuring.clj
+++ src/koans/14_destructuring.clj
@@ -1,41 +1,57 @@
+(def join clojure.string/join)
+
 (def test-address
   {:street-address "123 Test Lane"
    :city "Testerville"
    :state "TX"})
 
 (meditations
+
   "Destructuring is an arbiter: it breaks up arguments"
-  (= __ ((fn [[a b]] (str b a))
+  (= ":bar:foo" ((fn [[a b]] (str b a))
          [:foo :bar]))
 
   "Whether in function definitions"
   (= (str "First comes love, "
           "then comes marriage, "
           "then comes Clojure with the baby carriage")
-     ((fn [[a b c]] __)
-      ["love" "marriage" "Clojure"]))
+     ((fn [[a b c]]
+         (str "First comes " a ", "
+              "then comes " b ", "
+              "then comes " c " with the baby carriage")
+      ) ["love" "marriage" "Clojure"]))
 
   "Or in let expressions"
   (= "Rich Hickey aka The Clojurer aka Go Time aka Macro Killah"
      (let [[first-name last-name & aliases]
            (list "Rich" "Hickey" "The Clojurer" "Go Time" "Macro Killah")]
-       __))
+       (join " aka " (cons (str first-name " " last-name) aliases))))
 
   "You can regain the full argument if you like arguing"
-  (= {:original-parts ["Steven" "Hawking"] :named-parts {:first "Steven" :last "Hawking"}}
+  (= {:original-parts ["Steven" "Hawking"]
+      :named-parts {:first "Steven" :last "Hawking"}}
      (let [[first-name last-name :as full-name] ["Steven" "Hawking"]]
-       __))
+       {:original-parts full-name
+        :named-parts {:first first-name :last last-name}}))
 
   "Break up maps by key"
   (= "123 Test Lane, Testerville, TX"
-     (let [{street-address :street-address, city :city, state :state} test-address]
-       __))
+     (let [{street-address :street-address
+            city :city
+            state :state} test-address]
+       (join ", " [street-address city state])))
 
   "Or more succinctly"
   (= "123 Test Lane, Testerville, TX"
-     (let [{:keys [street-address __ __]} test-address]
-       __))
+     (let [{:keys [street-address city state]} test-address]
+       (join ", " [street-address city state])))
 
   "All together now!"
   (= "Test Testerson, 123 Test Lane, Testerville, TX"
-     (___ ["Test" "Testerson"] test-address)))
+     ((fn [[fname lname] {:keys [street-address city state]}]
+          (join ", " [(str fname " " lname) street-address city state])
+      ) ["Test" "Testerson"] test-address)
+  )
+
+)
+
